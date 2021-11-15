5c77f4f8d82bfca81244cae2d19621ec7bd20912
Cat-Hanbit <gysophila001@gmail.com>
2021-11-15 15:30:24 +0900

    Slave branch 추가

--- HelloWorld/src/start.java
+++ HelloWorld/src/start.java
@@ -2,7 +2,8 @@
 public class start {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World! ");
+		System.out.println("Hello World!");
+		System.out.println("Slave Branch");
 
 	}
 
