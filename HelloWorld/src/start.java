6f194c47362ff8af6e30c5816caf304f31f45a0c
JBGeum <94034050+JBGeum@users.noreply.github.com>
2021-11-15 15:57:24 +0900

    Update start.java
    Github 수정

--- HelloWorld/src/start.java
+++ HelloWorld/src/start.java
@@ -4,6 +4,9 @@ public class start {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Slave Branch");
+		
+		System.out.println("Github 수정");
+
 
 	}
 
